331947fb7bcf7edaa47204412e14426bf437bed9
elbeicktalat <talatelbeick@gmail.com>
2023-09-04 19:42:17 +0200

    add annotation to Scene.viewModel & build

--- lib/src/interfaces/scene.dart
+++ lib/src/interfaces/scene.dart
@@ -1,7 +1,8 @@
 import 'package:clean_provider/clean_provider.dart';
 import 'package:clean_provider/src/_internal/internal.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart' show StatelessWidget;
+import 'package:flutter/material.dart'
+    show BuildContext, StatelessWidget, Widget;
 
 abstract class Scene<T extends ViewModel> extends StatelessWidget
     with BaseSceneMixin<T> {
@@ -11,7 +12,14 @@ abstract class Scene<T extends ViewModel> extends StatelessWidget
   });
 
   @override
+  @protected
+  @nonVirtual
   final T viewModel;
+
+  @override
+  @protected
+  @nonVirtual
+  Widget build(BuildContext context) => super.build(context);
 
   @override
   void debugFillProperties(DiagnosticPropertiesBuilder properties) {
