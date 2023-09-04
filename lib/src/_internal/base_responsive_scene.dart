0a08d19bdcd00825017063ed7ec34fb4f4d1547f
elbeicktalat <talatelbeick@gmail.com>
2023-09-04 19:37:23 +0200

    add Responsive version of Scene

---
+++ lib/src/_internal/base_responsive_scene.dart
@@ -0,0 +1,112 @@
+part of internal;
+
+@internal
+@immutable
+mixin ResponsiveSceneMixin<T extends BaseViewModel> on BaseSceneMixin<T> {
+  @protected
+  ResponsiveSceneData get screen;
+
+  @protected
+  bool get alwaysUseBuilder;
+
+  @override
+  @protected
+  @nonVirtual
+  Widget build(BuildContext context) {
+    viewModel._context = context;
+    screen._context = context;
+    Widget? widget;
+    if (alwaysUseBuilder) {
+      widget = builder();
+      if (widget != null) return widget;
+    }
+    if (screen.isDesktop) {
+      widget = desktop() ?? widget;
+      if (widget != null) return widget;
+    }
+    if (screen.isTablet) {
+      widget = tablet() ?? desktop();
+      if (widget != null) return widget;
+    }
+    if (screen.isPhone) {
+      widget = phone() ?? tablet() ?? desktop();
+      if (widget != null) return widget;
+    }
+    return watch() ?? phone() ?? tablet() ?? desktop() ?? builder()!;
+  }
+
+  @override
+  @protected
+  Widget? builder() => null;
+
+  @protected
+  Widget? desktop() => null;
+
+  @protected
+  Widget? phone() => null;
+
+  @protected
+  Widget? tablet() => null;
+
+  @protected
+  Widget? watch() => null;
+}
+
+final class ResponsiveSceneData {
+  ResponsiveSceneData(this.settings);
+
+  final ResponsiveSceneSettings settings;
+
+  late BuildContext _context;
+
+  bool get _isDesktop =>
+      Platform.isWindows || Platform.isMacOS || Platform.isLinux;
+
+  double get width => MediaQuery.of(_context).size.width;
+
+  double get height => MediaQuery.of(_context).size.height;
+
+  /// Whether [screenType] is [ScreenType.desktop]
+  bool get isDesktop => screenType == ScreenType.desktop;
+
+  /// Whether [screenType] is [ScreenType.tablet]
+  bool get isTablet => screenType == ScreenType.tablet;
+
+  /// Whether [screenType] is [ScreenType.phone]
+  bool get isPhone => screenType == ScreenType.phone;
+
+  /// Whether [screenType] is [ScreenType.watch]
+  bool get isWatch => screenType == ScreenType.watch;
+
+  double get _getDeviceWidth {
+    if (_isDesktop) {
+      return width;
+    }
+    return MediaQuery.of(_context).size.shortestSide;
+  }
+
+  /// Return the current [ScreenType].
+  ScreenType get screenType {
+    if (_getDeviceWidth >= settings.desktopChangePoint) return ScreenType.desktop;
+    if (_getDeviceWidth >= settings.tabletChangePoint) return ScreenType.tablet;
+    if (_getDeviceWidth < settings.watchChangePoint) return ScreenType.watch;
+    return ScreenType.phone;
+  }
+
+  /// Return widget according to screen type.
+  ///
+  /// If the [screenType] is [ScreenType.desktop] and `desktop` object is null, the `tablet` object will be returned.
+  /// If the [screenType] is [ScreenType.tablet] and `tablet` object is null, the `mobile` object will be returned.
+  /// If the [screenType] is [ScreenType.phone] and `phone` object is null, the `watch` object will be returned, also when it is null.
+  T? responsiveValue<T>({
+    T? phone,
+    T? tablet,
+    T? desktop,
+    T? watch,
+  }) {
+    if (isDesktop && desktop != null) return desktop;
+    if (isTablet && tablet != null) return tablet;
+    if (isPhone && phone != null) return phone;
+    return watch;
+  }
+}
---
+++ lib/src/interfaces/responsive_scene.dart
@@ -0,0 +1,67 @@
+import 'package:clean_provider/src/_internal/internal.dart';
+import 'package:clean_provider/src/interfaces/view_model.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+abstract class ResponsiveScene<T extends ViewModel> extends StatelessWidget
+    with BaseSceneMixin<T>, ResponsiveSceneMixin<T> {
+  ResponsiveScene(
+    this.viewModel, {
+    this.alwaysUseBuilder = false,
+    ResponsiveSceneSettings settings = const ResponsiveSceneSettings(),
+    super.key,
+  }) : screen = ResponsiveSceneData(settings);
+
+  @override
+  @protected
+  @nonVirtual
+  final T viewModel;
+
+  @override
+  final bool alwaysUseBuilder;
+
+  @override
+  final ResponsiveSceneData screen;
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(DiagnosticsProperty<T>('viewModel', viewModel));
+    properties
+        .add(DiagnosticsProperty<bool>('alwaysUseBuilder', alwaysUseBuilder));
+    properties.add(DiagnosticsProperty<ResponsiveSceneData>('screen', screen));
+  }
+}
+
+final class ResponsiveSceneSettings {
+  const ResponsiveSceneSettings({
+    this.desktopChangePoint = 1200,
+    this.tabletChangePoint = 600,
+    this.watchChangePoint = 300,
+  });
+
+  /// When the width is greater or equal as this value,
+  /// the display will be set as [ScreenType.desktop].
+  final double desktopChangePoint;
+
+  /// When the width is greater or equal as this value,
+  /// the display will be set as [ScreenType.tablet].
+  ///
+  /// Also when the width is greater than [watchChangePoint] and smaller than this value,
+  /// the display will be [ScreenType.phone].
+  final double tabletChangePoint;
+
+  /// When the width is greater or equal as this value,
+  /// the display will be set as [ScreenType.watch].
+  ///
+  /// Also when the width is greater than [tabletChangePoint] and smaller than this value,
+  /// the display will be [ScreenType.phone].
+  final double watchChangePoint;
+}
+
+enum ScreenType {
+  watch,
+  phone,
+  tablet,
+  desktop,
+}
