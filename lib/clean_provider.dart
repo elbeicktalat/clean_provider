a33edc04f73c2f3dd4a65ba0a059586340c2be38
elbeicktalat <talatelbeick@gmail.com>
2023-09-06 15:31:16 +0200

    add copyright

--- lib/clean_provider.dart
+++ lib/clean_provider.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2023. Talat El Beick. All rights reserved.
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file.
+
 library clean_provider;
 
 export 'package:clean_provider/src/interfaces/responsive_scene.dart';
--- lib/src/interfaces/scene.dart
+++ lib/src/interfaces/scene.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2023. Talat El Beick. All rights reserved.
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file.
+
 import 'package:clean_provider/clean_provider.dart';
 import 'package:clean_provider/src/_internal/internal.dart';
 import 'package:flutter/foundation.dart';
